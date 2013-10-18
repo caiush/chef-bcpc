4695c22dfc8a8fb47036ca6893f52ad0b055b74c
Caius Howcroft <chowcroft@bloomberg.net>
2013-10-18 10:50:51 -0400

    Fixed a typo in the help for check_floats.py

--- cookbooks/bcpc/files/default/check_floats.py
+++ cookbooks/bcpc/files/default/check_floats.py
@@ -1,22 +1,26 @@
 #!/usr/bin/env python
 
-"""
+"""Checks the nova and ip addr agree on the floating ips that should be assigned
+to this host. 
 
 """
+
+import os
+import sys
+import netaddr
+import subprocess 
+
+from novaclient.v1_1 import client
 
 def get_nova_floats(username, password, tenant, auth_url):
    """Query nova for the locally hosted floats. Results a *set*
    of ips. 
    """
-   from novaclient.v1_1 import client
    nova = client.Client(username, password, tenant, auth_url)
    return set( [ str(a.address) for a in nova.floating_ips_bulk.list(os.uname()[1]) ] )
             
-
 def get_iptables_floats(cidr, interface):
    """Parse ip addr, returns a *set* of all ips on interface"""
-   import netaddr
-   import subprocess 
    ipn = netaddr.IPNetwork(cidr)
    sp = subprocess.Popen(["ip", "addr", "show", interface], stdout=subprocess.PIPE)
    sp.wait()
@@ -35,10 +39,7 @@ def get_iptables_floats(cidr, interface):
       
 
 if __name__ == '__main__':
-   from optparse import OptionParser 
-   import sys
-   import os
-   
+   from optparse import OptionParser    
    parser = OptionParser(usage="%prog [options]", version="%prog 0.1")
    parser.add_option("-u", "--os_username", dest="os_username", type="string",
                      default=os.environ.get("OS_USERNAME", ""),                     
@@ -48,10 +49,10 @@ if __name__ == '__main__':
                      help="The openstack password [**from env**]")
    parser.add_option("-t", "--os_tenant_name", dest="os_tenant_name", type="string",
                      default=os.environ.get("OS_TENANT_NAME", "AdminTentant"),                     
-                     help="The openstack Tenant Name [%default]")
+                     help="The openstack tenant name [%default]")
    parser.add_option("-l", "--os_auth_url", dest="os_auth_url", type="string",
                      default=os.environ.get("OS_AUTH_URL", "https://127.0.0.1"),                     
-                     help="The openstack Tenant Name [%default]")
+                     help="The openstack auth api end point [%default]")
 
    parser.add_option("-c", "--cidr", dest="cidr", type="string",
                      default="",
